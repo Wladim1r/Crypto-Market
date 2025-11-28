f98dcb6eae747bab822fb56344403f6b98debea9
Wladim1r <volodoy53728@mail.ru>
2025-11-28 20:25:16 +0300

    init Profile microservice

---
+++ Profile/internal/models/models.go
@@ -0,0 +1,19 @@
+// Package models
+package models
+
+import "github.com/shopspring/decimal"
+
+type UserProfile struct {
+	ID     uint   `gorm:"primaryKey"`
+	UserID uint   `gorm:"not null"`
+	Coins  []Coin `gorm:"foreignKey:UserProfileID"`
+}
+
+type Coin struct {
+	ID       uint            `gorm:"primaryKey"`
+	Symbol   string          `gorm:"not null"`
+	Quantity decimal.Decimal `gorm:"type:decimal(20,8);not null"`
+
+	UserProfileID uint
+	UserProfile   UserProfile `gorm:"constraint:OnDelete:CASCADE;"`
+}
