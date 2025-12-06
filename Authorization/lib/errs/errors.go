93644d24fd267b80c11e15d42ae20f5f3292d827
Tonic56 <qtonic56@gmail.com>
2025-12-06 23:19:00 +0300

    add new Errors

--- Authorization/lib/errs/errors.go
+++ Authorization/lib/errs/errors.go
@@ -4,8 +4,11 @@ package errs
 import "errors"
 
 var (
-	ErrDB           = errors.New("database error")
-	ErrRecordingWNC = errors.New("recording wasn't created")
-	ErrRecordingWND = errors.New("recording wasn't deleted")
-	ErrRecordingWNF = errors.New("recording wasn't found")
+	ErrDB              = errors.New("database error")
+	ErrRecordingWNC    = errors.New("recording wasn't created")
+	ErrRecordingWND    = errors.New("recording wasn't deleted")
+	ErrRecordingWNF    = errors.New("recording wasn't found")
+	ErrUserExists      = errors.New("user already exists")
+	ErrInvalidToken    = errors.New("invalid or expired token")
+	ErrSessionNotFound = errors.New("session not found")
 )
